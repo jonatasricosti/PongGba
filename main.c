674aca6cb1c39d0c2c96feb000556ae882ec74be
jonatasricosti <jonatas-ricosti@hotmail.com>
2025-05-06 18:24:10 -0300

    adicionado todas as funções de desenho do jogo

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
+const int screen_width  = 240;
+const int screen_height = 160;
+
 #define BG2 1024
 
 // algumas cores
 unsigned short blue = 0x7C00;
 unsigned short green = 0x3E0;
 unsigned short red = 0x1F;
-unsigned short white = 0x7FFF;
 unsigned short yellow = 0x3FF;
+unsigned short white = 0x7FFF;
+unsigned short black = 0;
 
 // use essa função pra ativar o mode de video
 void SetVideoMode(int mode)
@@ -20,6 +24,50 @@ unsigned short *vram = (unsigned short *)0x6000000;
 void DrawPixel(int x, int y, unsigned short color)
 {
 	vram[x+y*240] = color;
+}
+
+
+// use essa função pra colocar cor de fundo na tela mode 3 e 5
+void BackgroundColor(unsigned short color)
+{
+	int i;
+	for(i = 0; i < screen_width*screen_height; i++)
+	{
+		vram[i] = color;
+	}
+}
+
+#include "font.h"
+// use essa função pra desenhar um caractere no mode 3 precisa de font.h e só tem letras maiúsculas
+void DrawCharMode3(int left, int top, char letter, unsigned short color)
+{
+	int x,y,draw;
+	
+	for(y = 0; y < 8; y++)
+	{
+		for(x = 0; x < 8; x++)
+		{
+			// pegue um pixel do font char
+			draw = font[ (letter-32)*64 + x+8*y];
+			
+			// se pixer = 1, então desenhe
+			if(draw == 1)
+			{
+				DrawPixel(left + x, top + y, color);
+			}
+		}
+	}
+}
+
+// use essa função pra desenhar um texto no mode 3 precisa de font.h e só tem letras maiúsculas
+void DrawTextMode3(int left, int top, char *text, unsigned short color)
+{
+	int pos = 0;
+	while(*text)
+	{
+		DrawCharMode3(left+pos,top,*text++,color);
+		pos = pos+8;
+	}
 }
 
 // use essa função pra desenhar um retângulo na tela do game boy advance mode3
@@ -48,9 +96,19 @@ int main()
 SetVideoMode(3 | BG2);
 
 
-DrawRect(20,80,8,20,blue);
+BackgroundColor(white);
 
-DrawRect(240-20-2*8,80,8,20,red);
+// game loop
+while(1)
+{
+	DrawRect(20,80,8,28,blue);
+	DrawRect(240-20-2*8,80,8,28,red);
+	
+	DrawTextMode3(1,20,"PROGRAMAR EH LEGAL",black);
+	DrawTextMode3(1,30,"C E C++ EH MUITO LEGAL",green);
+	DrawTextMode3(1,40,"PONG PARA O GAME BOY ADVANCE",yellow);
+}
+
 
 return 0;
 }
